bf1512a936da097aca437334ff808884f0fd7c36
glassy-sky-lisong <1990691193@qq.com>
2021-10-23 17:36:39 +0800

    动态作用域-1

---
+++ 执行上下文与作用域/动态作用域.js
@@ -0,0 +1,22 @@
+// 全局作用域
+var value = 1;
+
+function foo() {
+    // foo 函数作用域
+  console.log(value);
+}
+
+function bar() {
+    // bar 函数作用域
+  var value = 2;
+  var obj = {
+      value: 11
+  }
+
+//   谁调用了bar函数
+  console.log(this)
+  foo.bind(obj)();
+}
+
+bar();
+
