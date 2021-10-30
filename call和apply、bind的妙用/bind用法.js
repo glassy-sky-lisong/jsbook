e73f197e2bccbaf6f47f871bbb913f8d514c6f2d
glassy-sky-lisong <1990691193@qq.com>
2021-10-30 11:00:01 +0800

    4

--- call和apply、bind的妙用/bind用法.js
+++ call和apply、bind的妙用/bind用法.js
@@ -3,11 +3,12 @@ var obj = {
 }
 
 function cook(a) {
-    console.log(`${this.name} cook`)
+    console.log(`${this.name} cook`, this)
     console.log(a)
     this.run = () => {
         console.log('run')
     }
+    this.age = 12
 }
 
 var newFunc = cook.bind(obj, 1)
@@ -24,9 +25,13 @@ var newFuncObj = new newFunc()
 
 console.log(newFuncObj)
 
-newFuncObj.run()
-
-// 当绑定函数成为i
-
 // 绑定函数使用new之后
 console.log('after', obj)
+
+
+newFuncObj.run()
+
+// 绑定函数自动适应于使用 new 操作符去构造一个由目标函数创建的新实例。 
+// 当一个绑定函数是用来构建一个值的，原来提供的 this 就会被忽略。
+// 不过提供的参数列表仍然会插入到构造函数调用时的参数列表之前。
+
